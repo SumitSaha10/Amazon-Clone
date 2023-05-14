9ecfa043a07e52693146d8045e3af76b05596da0
Sumit Saha <sumitsaha1233@gmail.com>
2023-05-14 20:37:03 +0530

    Login component added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import './App.css';
 import Checkout from './components/Checkout';
 import Header from './components/Header';
 import Home from './components/Home';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LogIn from './components/LogIn';
 function App() {
+
   return (
     <div className="App">
       <Router>
-      {/* header */}
-      <Header/>
-      <Routes>
-      {/* home */}
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/checkout' element={<Checkout/>}/>
-      </Routes>
-    </Router>
+        {/* header */}
+        <Header />
+        <Routes>
+          {/* home */}
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/checkout' element={<Checkout />} />
+          <Route exact path='/login' element={<LogIn />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from './firebase'
 const firebaseConfig = {
   apiKey: "AIzaSyBL9wA7T6pQ9GnaKiim1AK1rv8hQ3GrZto",
   authDomain: "clone-cabff.firebaseapp.com",
@@ -8,3 +9,8 @@ const firebaseConfig = {
   appId: "1:1053434821692:web:6ebcbe3dfecb1737eb5739",
   measurementId: "G-Z3N6YW6NXC"
 };
+
+
+const auth = firebase.auth();
+
+export default auth;
